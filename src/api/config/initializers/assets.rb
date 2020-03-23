d7a8e76727fc6c3efe4df7c13918bd14ffb98cb5
David Kang <dkang@suse.com>
2020-03-23 15:34:28 +0100

    test

--- src/api/config/initializers/assets.rb
+++ src/api/config/initializers/assets.rb
@@ -8,6 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += ['webui.js',
-                                               'webui/obs_factory/application.css',
-                                               'webui2/cm2/*.js']
+# Rails.application.config.assets.precompile += ['webui.js',
+#                                                'webui/obs_factory/application.css',
+#                                                'webui2/cm2/*.js']
