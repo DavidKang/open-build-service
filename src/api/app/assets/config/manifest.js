b68022b94758b5e562f9533fad648166f48a6518
David Kang <dkang@suse.com>
2020-03-23 15:47:11 +0100

    manifest

--- src/api/app/assets/config/manifest.js
+++ src/api/app/assets/config/manifest.js
@@ -4,6 +4,7 @@
 //= link webui/application.js
 //= link webui2/webui2.css
 //= link webui2/application.js
+//= link webui/obs_factory/application.css
 //
 // CODEMIRROR
 //= link webui2/cm2/show-prjconf.js
