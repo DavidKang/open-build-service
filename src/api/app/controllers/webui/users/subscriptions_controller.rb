1ebc8519ffe2ea6aa7955ea39215462ec0d754f9
David Kang <dkang@suse.com>
2020-04-13 10:44:58 +0200

    Adapt subscription form view to handle new channels
    Co-authored-by: Daniel Donisa <daniel.donisa@suse.com>

--- src/api/app/controllers/webui/users/subscriptions_controller.rb
+++ src/api/app/controllers/webui/users/subscriptions_controller.rb
@@ -20,9 +20,9 @@ class Webui::Users::SubscriptionsController < Webui::WebuiController
     end
 
     subscriptions_form.update!(params[:subscriptions]) if params[:subscriptions]
-    flash[:success] = 'Notifications settings updated'
+    flash.now[:success] = 'Notifications settings updated'
   rescue ActiveRecord::RecordInvalid
-    flash[:error] = 'Notifications settings could not be updated due to an error'
+    flash.now[:error] = 'Notifications settings could not be updated due to an error'
   ensure
     respond_to do |format|
       format.html { redirect_to action: :index }
