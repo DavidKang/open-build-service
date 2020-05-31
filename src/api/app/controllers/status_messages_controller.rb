c1bad6cac7982edbed6ad94f39c0f47d085b56e3
Saray Cabrera Padrón <scabrerapadron@suse.de>
2020-05-31 17:59:50 +0200

    Adapt announcement endpoints to act as status messages
    Announcement functionality has been removed but not its endpoints to
    keep backward compatibility. Instead, they will end up in status
    messages actions and views.
    
    Modifications:
      - Adapt status message policy.
      - Rename 'message' to 'status_message'.
      - Add update action to status messages to be able to keep update
        announcement's endpoint.
      - Use absolute routes inside status messages partials.
    
    Co-authored-by: Victor Pereira <vpereira@suse.com>

--- src/api/app/controllers/status_messages_controller.rb
+++ src/api/app/controllers/status_messages_controller.rb
@@ -1,5 +1,6 @@
 class StatusMessagesController < ApplicationController
-  before_action :require_admin, only: [:create, :destroy]
+  before_action :require_admin, only: [:create, :update, :destroy]
+  before_action :set_status_message, except: [:index, :create]
 
   def index
     @messages = StatusMessage.alive.limit(params[:limit]).order('created_at DESC').includes(:user)
@@ -12,22 +13,32 @@ class StatusMessagesController < ApplicationController
 
   def create
     status_message = StatusMessage.from_xml(validate_status_message)
-
     authorize status_message
-
     status_message.save!
-
     render_ok
   end
 
+  def update
+    authorize @status_message
+    if @status_message.update(validate_status_message)
+      render_ok
+    else
+      render_error message: @status_message.errors.full_messages,
+                   status: 400, errorcode: 'invalid_status_message'
+    end
+  end
+
   def destroy
-    status_message = StatusMessage.find(params[:id])
-    authorize status_message
-    status_message.delete
+    authorize @status_message
+    @status_message.delete
     render_ok
   end
 
   private
+
+  def set_status_message
+    @status_message = StatusMessage.find(params[:id])
+  end
 
   # TODO: make it more robust
   def validate_status_message
