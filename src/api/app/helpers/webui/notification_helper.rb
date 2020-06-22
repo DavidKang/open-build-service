be547ee43b32eea7d77622c658d0c3ed1c8672cd
Saray Cabrera Padrón <scabrerapadron@suse.de>
2020-06-22 14:49:22 +0200

    Redesign notifications view to be more compact
    A notification has now a more compact aspect. Specially useful on
    small screens. Using icons, colors and avatars makes it easier to
    understand at a glance.
    
    - An icon represents each kind of notification: request or comment.
    - Review notifications are going to be replaced by requests
      notifications with "in review" state.
    - Display the state of the request clearly.
    - Link to the comments box and not the comment itself.
    - Display all users involved in notifications
      - In case of request's notifications, it also displays all the
        reviewers including groups, projects and packages.
    
    Co-authored-by: David Kang <dkang@suse.com>
    Co-authored-by: Eduardo Navarro <enavarro@suse.com>

--- src/api/app/helpers/webui/notification_helper.rb
+++ src/api/app/helpers/webui/notification_helper.rb
@@ -1,4 +1,6 @@
 module Webui::NotificationHelper
+  MAX_VISIBLE_AVATARS = 6
+
   def link_to_all
     parameters = params[:type] ? { type: params[:type] } : {}
     if params['show_all'] # already showing all
@@ -14,6 +16,98 @@ module Webui::NotificationHelper
       concat(link_text)
       concat(tag.span(amount, class: "badge #{badge_color(filter_item)} align-text-top ml-2")) if amount && amount.positive?
     end
+  end
+
+  def notification_icon(notifiable_type)
+    return image_tag('request_icon', size: 18, title: 'Request notification') if notifiable_type == 'BsRequest'
+
+    capture do
+      tag.i(class: 'fas fa-comments mr-1', title: 'Comment notification')
+    end
+  end
+
+  def request_state(notification)
+    return unless notification.notifiable_type == 'BsRequest'
+
+    state = notification.notifiable.state
+    color = request_badge_color(state)
+
+    capture do
+      tag.span(state, class: "badge badge-#{color}")
+    end
+  end
+
+  def request_badge_color(state)
+    case state
+    when :review, :new
+      'secondary'
+    when :declined, :revoke
+      'danger'
+    when :superseded
+      'warning'
+    when :accepted
+      'success'
+    else
+      'dark'
+    end
+  end
+
+  def notification_link(notification)
+    link_text = case notification.event_type
+                # TODO: add 'Event::ReviewWanted' when converted to BsRequest notification
+                when 'Event::RequestStatechange', 'Event::RequestCreate'
+                  "##{notification.notifiable.number}"
+                when 'Event::CommentForRequest'
+                  "Request ##{notification.notifiable.commentable.number}"
+                when 'Event::CommentForProject'
+                  notification.notifiable.commentable.name
+                when 'Event::CommentForPackage'
+                  commentable = notification.notifiable.commentable
+                  "#{commentable.project.name} / #{commentable.name}"
+                end
+
+    link_to(link_text, notification.link_to_notification_target, class: 'mx-1 text-word-break-all')
+  end
+
+  def read_unread_link(notification)
+    title, icon = notification.unread? ? ['Mark as "Read"', 'fa-check'] : ['Mark as "Unread"', 'fa-undo']
+
+    link_to(my_notification_path(id: notification),
+            method: :put, class: 'btn btn-sm btn-outline-success px-3', title: title) do
+      concat(tag.i(class: "fas #{icon}"))
+    end
+  end
+
+  # A limited number of avatars are going to be displayed
+  def request_tags(request)
+    tags = []
+
+    # Request's reviewers
+    if request.state == :review
+      open_reviews = request.reviews.where(state: :new)
+      open_reviews.limit(MAX_VISIBLE_AVATARS).each do |review|
+        tags << reviewers_tags(review)
+      end
+    end
+
+    if tags.size < MAX_VISIBLE_AVATARS
+      # Request's creator
+      tags << user_avatar(User.find_by(login: request.creator))
+    else
+      # Counter
+      hidden = (1 + open_reviews.size - MAX_VISIBLE_AVATARS)
+      tags << tag.span("+#{hidden}", class: 'rounded-circle bg-light border border-gray-400 avatars-counter',
+                                     title: "#{hidden} more users involved")
+    end
+    tags.reverse
+  end
+
+  def user_avatar(user)
+    image_tag_for(user, size: 23, custom_class: 'rounded-circle bg-light border border-gray-400')
+  end
+
+  def simulated_avatar(icon_color, title)
+    tag.span(class: "fa #{icon_color} rounded-circle bg-light border border-gray-400 simulated-avatar", title: title)
   end
 
   private
@@ -36,5 +130,12 @@ module Webui::NotificationHelper
 
   def badge_color(filter_item)
     notification_filter_active?(filter_item) ? 'badge-light' : 'badge-primary'
+  end
+
+  def reviewers_tags(review)
+    return user_avatar(User.find_by(login: review.by_user)) if review.by_user
+    return user_avatar(Group.find_by(title: review.by_group)) if review.by_group
+    return simulated_avatar('fa-archive text-warning', "Package #{review.by_project}/#{review.by_package}") if review.by_package
+    return simulated_avatar('fa-cubes text-secondary', "Project #{review.by_project}") if review.by_project
   end
 end
--- src/api/app/presenters/notification_presenter.rb
+++ src/api/app/presenters/notification_presenter.rb
@@ -11,32 +11,21 @@ class NotificationPresenter < SimpleDelegator
     when 'Event::ReviewWanted'
       Rails.application.routes.url_helpers.request_show_path(@model.notifiable.bs_request.number)
     when 'Event::CommentForRequest'
-      Rails.application.routes.url_helpers.request_show_path(@model.notifiable.commentable.number, anchor: "comment-#{@model.notifiable_id}")
+      Rails.application.routes.url_helpers.request_show_path(@model.notifiable.commentable.number, anchor: 'comments-list')
     when 'Event::CommentForProject'
-      Rails.application.routes.url_helpers.project_show_path(@model.notifiable.commentable, anchor: "comment-#{@model.notifiable_id}")
+      Rails.application.routes.url_helpers.project_show_path(@model.notifiable.commentable, anchor: 'comments-list')
     when 'Event::CommentForPackage'
       Rails.application.routes.url_helpers.package_show_path(package: @model.notifiable.commentable,
                                                              project: @model.notifiable.commentable.project,
-                                                             anchor: "comment-#{@model.notifiable_id}")
+                                                             anchor: 'comments-list')
     else
       ''
     end
   end
 
-  def notification_badge
-    case @model.event_type
-    when 'Event::RequestStatechange', 'Event::RequestCreate'
-      'Request'
-    when 'Event::ReviewWanted'
-      'Review'
-    when 'Event::CommentForRequest', 'Event::CommentForProject', 'Event::CommentForPackage'
-      'Comment'
-    end
-  end
-
   def excerpt
     text =  case @model.notifiable_type
-            when 'Request'
+            when 'BsRequest'
               @model.notifiable.description
             when 'Review'
               @model.notifiable.reason
@@ -46,5 +35,9 @@ class NotificationPresenter < SimpleDelegator
               ''
             end
     text.to_s.truncate(100)
+  end
+
+  def kind_of_request
+    BsRequest.actions_summary(@model.event_payload) if @model.notifiable_type == 'BsRequest'
   end
 end
